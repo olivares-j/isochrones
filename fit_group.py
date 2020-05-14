636b2d0616daf8b60341dcb93c8a4d3e2ded7896
olivares-j <avijer@gmail.com>
2020-05-14 20:07:10 +0200

    Remove AllWise photometry

--- fit_group.py
+++ fit_group.py
@@ -24,7 +24,7 @@ dosmass_unc  = ["e_Jmag","e_Hmag","e_Kmag"]
 allwise      = ["W1","W2","W3"]
 allwise_obs  = ["W1mag","W2mag","W3mag"]
 allwise_unc  = ["e_W1mag","e_W2mag","e_W3mag"]
-bands        = ['BP','G','RP','J','H','K','W1','W2','W3']
+bands        = ['BP','G','RP','J','H','K']#,'W1','W2','W3']
 parameters   = ["age","mass","distance","AV"]
 #---------------------------------------------------------------
 
@@ -34,10 +34,15 @@ mist = get_ichrone('mist', bands=bands)
 
 #-------------- Transform lists -------------------------------
 bands_mag = [ band+"_mag" for band in bands]
-phot_obs   = sum([gaia_obs,dosmass_obs,allwise_obs],[])
-phot_unc   = sum([gaia_unc,dosmass_unc,allwise_unc],[])
-columns_data = sum([[identifier],gaia_obs,dosmass_obs,allwise_obs,
-							     gaia_unc,dosmass_unc,allwise_unc],[])
+# phot_obs   = sum([gaia_obs,dosmass_obs,allwise_obs],[])
+# phot_unc   = sum([gaia_unc,dosmass_unc,allwise_unc],[])
+# columns_data = sum([[identifier],gaia_obs,dosmass_obs,allwise_obs,
+# 							     gaia_unc,dosmass_unc,allwise_unc],[])
+
+phot_obs   = sum([gaia_obs,dosmass_obs],[])
+phot_unc   = sum([gaia_unc,dosmass_unc],[])
+columns_data = sum([[identifier],gaia_obs,dosmass_obs,gaia_unc,dosmass_unc],[])
+
 stats_names = sum([[identifier],["mean_"+p for p in parameters],
 					["std_"+p for p in parameters]],[])
 phot_obs.pop(0)
@@ -46,7 +51,7 @@ phot_unc.pop(0)
 
 #------------ Files -------------------------------
 dir_base   = "/raid/jromero/OCs/Taurus/"
-dir_base   = "/home/javier/Cumulos/Taurus/run_1/"
+# dir_base   = "/home/javier/Cumulos/Taurus/run_1/"
 file_data  = dir_base + "members.csv"
 dir_out    = dir_base + "Isochrones/"
 dir_chain  = dir_out  + "chains/"
@@ -91,15 +96,15 @@ for ID,datum in df.iterrows():
 			params[true] = (datum[obs],datum[unc])
 	#---------------------------------------------------------
 
-	#---------------- AllWISE ----------------------------------
-	for true,obs,unc in zip(allwise,allwise_obs,allwise_unc):
-		if np.isfinite(datum[obs]):
-			if not np.isfinite(datum[unc]):
-				datum[unc] = 0.2*datum[obs]
+	# #---------------- AllWISE ----------------------------------
+	# for true,obs,unc in zip(allwise,allwise_obs,allwise_unc):
+	# 	if np.isfinite(datum[obs]):
+	# 		if not np.isfinite(datum[unc]):
+	# 			datum[unc] = 0.2*datum[obs]
 
-		#-------- Add only if observed -------------
-			params[true] = (datum[obs],datum[unc])
-	#---------------------------------------------------------
+	# 	#-------- Add only if observed -------------
+	# 		params[true] = (datum[obs],datum[unc])
+	# #---------------------------------------------------------
 
 	
 	#======================================================
